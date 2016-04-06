aaced229256007c818660367439c780acc67c01a
Madison Maloof <maddymaloof@gmail.com>
2016-04-05 20:20:29 -0500

    initial commit

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,19 @@
+class EmployeesController < ApplicationController
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+    render 'index.html.erb'
+  end
+
+  def show 
+    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    render 'show.html.erb'
+  end
+
+  def new 
+    render "new.html.erb"
+  end
+
+  def create
+    
+  end
+end
