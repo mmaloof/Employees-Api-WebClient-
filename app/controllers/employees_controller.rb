80e47429e26539efa36c8b09c90e19b8b15faf50
Madison Maloof <maddymaloof@gmail.com>
2016-04-09 17:04:06 -0500

    Added edit/update, delete features - PORO to be added next

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,7 +5,8 @@ class EmployeesController < ApplicationController
   end
 
   def show 
-    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    employee_hash = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    @employee = Employee.new(employee_hash)
     render 'show.html.erb'
   end
 
@@ -14,6 +15,38 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    
+    @employee = Unirest.post(
+      "http://localhost:3000/api/v1/employees", 
+      headers: {"Accept" => "application/json"}, 
+      parameters: {
+        first_name: params[:input_form_first_name], 
+        last_name: params[:input_form_last_name], 
+        email: params[:input_form_email] 
+      }
+    ).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
+  def edit
+    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}").body
+    render 'edit.html.erb'
+  end
+
+  def update
+    @employee = Unirest.patch(
+      "http://localhost:3000/api/v1/employees/#{params[:id]}.json",
+      headers: { "Accept" => "application/json" },
+      parameters: {
+        first_name: params[:input_form_first_name],
+        last_name: params[:input_form_last_name],
+        email: params[:input_form_email]
+      }
+    ).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
+  def destroy
+    Unirest.delete("http://localhost:3000/api/v1/employees/#{params[:id]}.json")
+    redirect_to "/employees"
   end
 end
